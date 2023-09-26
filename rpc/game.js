e7f32bbbfe48545236a598765f51dcff718860f3
Sai-Varun-M <143157157+Sai-Varun-M@users.noreply.github.com>
2023-09-26 16:23:48 +0530

    basic code for rock paper scissor in a web page

---
+++ rpc/game.js
@@ -0,0 +1,48 @@
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * 3);
+    return choices[randomIndex];
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerSelection) {
+        return "It's a tie! " + playerSelection + " equals " + computerSelection;
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+        return "You Win! " + playerSelection + " beats " + computerSelection;
+    } else {
+        return "You Lose! " + computerSelection + " beats " + playerSelection;
+    }
+}
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Enter your choice: Rock, Paper, or Scissors");
+        const computerSelection = getComputerChoice();
+        const result = playRound(playerSelection, computerSelection);
+        console.log(result);
+
+        if (result.includes("Win")) {
+            playerScore++;
+        } else if (result.includes("Lose")) {
+            computerScore++;
+        }
+    }
+
+    if (playerScore > computerScore) {
+        console.log("You win the game! Final Score: " + playerScore + " - " + computerScore);
+    } else if (computerScore > playerScore) {
+        console.log("Computer wins the game! Final Score: " + playerScore + " - " + computerScore);
+    } else {
+        console.log("It's a tie! Final Score: " + playerScore + " - " + computerScore);
+    }
+}
+
+game();
